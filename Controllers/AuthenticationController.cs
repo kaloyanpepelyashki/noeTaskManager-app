dfd50b533efbeb6a453815fa2b7a7a3bcc0c559c
kaloyanpepelyashki <kaloyan.pepelyashki@gmail.com>
2024-04-12 17:33:11 +0200

    Scaffold of the application + AuthenticationController
    Set up the scaffold of the application. The application currently is just an empty scaffold. Added two new views and one controller the views belong to. Added an AuthenticationController and Signin and Signup views.

---
+++ Controllers/AuthenticationController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace noeTaskManager_app.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        private readonly ILogger<AuthenticationController> _logger;
+
+        public AuthenticationController(ILogger<AuthenticationController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Signin()
+        {
+            return View();
+        }
+
+        public IActionResult Signup()
+        {
+            return View();
+        }
+    }
+}
